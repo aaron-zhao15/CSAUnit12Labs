47f55184eb6911863282f80d886f50bfcbcc579a
aaron-zhao15 <aaron.zhao15@gmail.com>
2019-03-14 00:07:40 -0700

    complete except for word

---
+++ src/unit12/Word.java
@@ -0,0 +1,23 @@
+package unit12;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+import static java.lang.System.*;
+
+public class Word implements Comparable<Word> {
+
+    private String word;
+
+    public Word(String s) {
+        word = s;
+    }
+
+    public int compareTo(Word rhs) {
+        return word.compareTo(rhs.toString());
+    }
+
+    public String toString() {
+        return word;
+    }
+}
